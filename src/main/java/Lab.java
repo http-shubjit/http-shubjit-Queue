a5a6143b93558adce554f11b180a5c0901d1cca9
Shubhajit <shubjitgithub@gmail.com>
2024-07-08 22:07:36 +0530

    Done

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,4 +1,5 @@
 import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Queue;
 
 /**
@@ -36,7 +37,7 @@ public class Lab {
      * Stack behavior.
      */
     public Deque<String> createQueue(){
-        return null;
+        return new LinkedList<String>();
     }
 
     /**
@@ -45,7 +46,7 @@ public class Lab {
      * @return the length of queue.
      */
     public int getSize(Queue<String> queue){
-        return 0;
+        return queue.size();
     }
 
     /**
@@ -54,8 +55,9 @@ public class Lab {
      * @param queue a Queue<String> object.
      * @param item a String that should be added to Queue.
      */
-    public void addToEndOfQueue(Queue<String> queue, String item){
+    public void addToEndOfQueue(Queue<String> queue, String item) {
 
+        queue.add(item);
     }
 
     /**
@@ -64,8 +66,9 @@ public class Lab {
      * @param queue a Queue<String> object.
      * @return the next String due to be removed (polled) from the Queue<String> (the oldest item in the queue.)
      */
-    public String removeFromStartOfQueue(Queue<String> queue){
-        return "";
+    public String removeFromStartOfQueue(Queue<String> queue) {
+        
+        return queue.poll();
     }
 
     /**
@@ -75,6 +78,6 @@ public class Lab {
      * @return the next String due to be removed (peeked) from the Queue<String> (the oldest item in the queue.)
      */
     public String getStartOfQueueWithoutRemoving(Queue<String> queue){
-        return "";
+        return queue.peek();
     }
 }
